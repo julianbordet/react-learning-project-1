95bfbdc20ef316642338611aacb13613642ce90b
jbdev <bordet.julian@gmail.com>
2022-07-19 15:22:03 -0300

    finalized structure landing page, included form

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,19 +1,29 @@
 import React from "react";
 import './MenuItem.css';
 
+/*<span>Amount:</span>
+<span>+ Add</span>*/
+
 const MenuItem = (props) =>{
 
     return(
         <React.Fragment>
             <div className='menu-item-container'>
                 <div className='first-column'>
-                    <span>{props.name}</span>
-                    <span>{props.description}</span>
-                    <span>{props.price}</span>
+                    <span className='menu-item-name'>{props.name}</span>
+                    <span className='menu-item-description'>{props.description}</span>
+                    <span className='menu-item-price'>${props.price}</span>
                 </div>
                 <div className='second-column'>
-                    <span>Amount:</span>
-                    <span>+ Add</span>
+                    <form className='item-form'>
+                        <div className='first-row'>
+                            <label htmlFor='amount' id='amount-label'>Amount</label>
+                            <input type='number' name='amount' id='amount'></input>
+                        </div>
+                        <div className='second-row'>
+                            <input type='submit' value='+ Add'></input>
+                        </div>
+                    </form>
                 </div>
             </div>
             <hr></hr>
