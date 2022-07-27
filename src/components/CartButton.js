b499c6de1501ac1ea98e199d44fc112064ba2f4d
jbdev <bordet.julian@gmail.com>
2022-07-27 18:03:29 -0300

    cartButton now correctly displays number of items on cart and updates as expected

--- src/components/CartButton.js
+++ src/components/CartButton.js
@@ -1,13 +1,29 @@
-import React from "react";
+import React, {useState, useContext, useEffect} from "react";
 import './CartButton.css'
+import { CartContext } from "./store/cart-context";
 
 const CartButton = (props) =>{
+
+    const [state, dispatch] = useContext(CartContext);
+    const [amount, setAmount] = useState(0);
+
+    
+
+    useEffect( () => {
+
+        let cartLength = 0;
+
+        cartLength = state.itemsOnCart.length;
+
+        setAmount(cartLength);
+
+    }, [state]);
 
     return(
         <a onClick={props.openModal} className='cart-container'>
             <i class="fa-solid fa-cart-shopping"></i>
             <span className='cart-text' href=''>Your Cart</span>
-            <span className='cart-items-counter'>0</span>
+            <span className='cart-items-counter'>{amount}</span>
         </a>
     )
 
